93b35517f8cdd052dd26f7818d7a310ffb99378c
MD. SAZIB HOSSEN RABBY <122990604+runcode42@users.noreply.github.com>
2023-01-30 13:04:57 +0600

    Add files via upload

---
+++ sena ready or not.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	int n;
+	
+	printf("Enter sena number : ");
+	scanf("%d",&n);
+	
+	int weapon[n];
+	
+	for(int i=0;i<n;i++){
+	    
+	    printf("enter wepon number : ");
+	    scanf("%d",&weapon[i]);
+	}
+	
+	int ready_sena=0;
+	int notready_sena=0;
+	
+	for(int i=0;i<n;i++){
+	    
+	    if(weapon[i]%2==0){
+	        
+	        ready_sena++;
+	    } else
+	     {
+	     	notready_sena++;
+	     }
+	}
+	
+	if(ready_sena>notready_sena){
+	    printf("READY FOR BATTLE ");
+	} else{
+	    printf("NOT READY");
+	}
+	
+	return 0;
+}
+
