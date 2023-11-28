1136fb9413819b96a7849e65e060b1009017c865
MD. SAZIB <122990604+mdsazibhossenrabby@users.noreply.github.com>
2023-11-28 12:20:14 +0600

    Add files via upload

---
+++ multiLine-in-printf.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("1. Multiple line in printf but single line in consol : \
+            *Physics \
+            *Math \
+            *Structure Programmming \n");
+
+    printf("\n");
+
+    printf("2. Multiple line in both printf and consol : \n \
+                * Physics \n \
+            * Math \n \
+                * Structure Programming \n");
+
+    printf("\n");
+
+    printf("3. Multiple line in both printf and consol : \n \
+                * Physics \n \
+                * Math \n \
+                * Structure Programming \n");
+
+    printf("\n");
+
+    printf("4. Multiple line  in printf and sigle line in consol : "
+            "* Physics "
+            "* Math "
+            "* Structure Programming ");
+
+    printf("\n \n");
+
+    //This method is very efficient
+
+    printf("5. Multiple line  in both printf and consol : \n"
+            "   * Physics \n"
+            "* Math \n"
+            "   * Structure Programming \n");
+
+
+    return 0;
+}
